8a379e73bf1add300a72c857df1489af7b7cc4d3
rajkumar kushwah <rajkumarkushwah3262@gmail.com>
2026-07-20 12:29:11 +0530

    , main file in this solve

--- hrms-frontend/src/main.tsx
+++ hrms-frontend/src/main.tsx
@@ -10,7 +10,7 @@ createRoot(document.getElementById('root')!).render(
     <ThemeProvider>
       <StrictMode>
         <App />
-      </StrictMode>,
+      </StrictMode>
     </ThemeProvider>
   </AuthProvider>
 )
