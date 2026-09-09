e5e4d512092d27959bcfec216e5b57fc2d1edcd7
rajkumar kushwah <rajkumarkushwah3262@gmail.com>
2026-09-09 14:54:32 +0530

    feat: implement PrismaClient with postgresql adapter and conditional ssl configuration

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -1,10 +1,29 @@
+// import { PrismaClient } from "@prisma/client";
+// import { PrismaPg } from "@prisma/adapter-pg";
+
+// export const prisma = new PrismaClient({
+//     adapter: new PrismaPg({
+//         connectionString: process.env.DATABASE_URL,
+//     }),
+    
+// });
+// console.log("Connected to postgresql successfully");
+
 import { PrismaClient } from "@prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
 
-export const prisma = new PrismaClient({
-    adapter: new PrismaPg({
-        connectionString: process.env.DATABASE_URL,
-    }),
-    
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL!,
+  ssl:
+    process.env.NODE_ENV === "production"
+      ? {
+          rejectUnauthorized: false,
+        }
+      : false,
 });
+
+export const prisma = new PrismaClient({
+  adapter,
+});
+
 console.log("Connected to postgresql successfully");
